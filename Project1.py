41d911b44cd6587e4fc6fb66af4ea4ee4e6f38bb
Mcdaddy-2pointoh <sharvil.dandekar@gmail.com>
2021-08-12 22:27:28 +0530

    project 1 complete

--- Project1.py
+++ Project1.py
@@ -1,15 +1,17 @@
 from tensorflow import keras
 import numpy as np
 
+
 # Defining and compiling a model
-model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model = keras.Sequential([keras.layers.Dense(units=2, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
 # Defining the array
-ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0, 9.0], dtype=float)
-xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0], dtype=float)
+x = np.array([ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9])
+y = np.array([10, 20, 25, 30, 40, 45, 40, 50, 60, 55])
 
 # Combining the two
-model.fit(xs, ys, epochs=500)
+model.fit(x, y, epochs=5000)
 
+print(model.predict([0.0]))
 print(model.predict([10.0]))
